eddbd3c19c6f597ae733df6e87c468d1a6c70b62
xzhbxm <xzhbbtv@126.com>
2019-04-27 17:03:09 +0800

    尝试创建结构目录有待完善

---
+++ src/js/init.js
@@ -0,0 +1,9 @@
+var timer = (new Date()).getTime();
+var timer2 = Date.now();
+
+console.log(timer, timer2);
+console.log((new Date()).getDate());
+
+// (function (window, document, undefined) {
+
+// })(window, document, undefined)
